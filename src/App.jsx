6be80fc32f15985681ddf8bf630098ce5b45e71a
annemassicotte <anne@cinetic.ca>
2023-06-25 00:01:04 -0400

    Ajout d'un H1 et ajustement de style CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Container from "./components/Container/Container";
 import Cards from "./components/Cards/Cards";
+import Title from "./components/Title/Title";
 
 function App() {
 	return (
-		<Container>
-			<Cards/>
-		</Container>
+		<>
+			<Container>
+				<Title className="centered">Anne's Pokemon Cards</Title>
+				<Cards />
+			</Container>
+		</>
 	);
 }
 
